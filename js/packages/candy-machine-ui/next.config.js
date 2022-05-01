d09badbe3a2d4bd239776b9d3fa3b9595889027c
Scott St. John <scott@tremulant.tech>
2022-04-30 18:27:14 -0700

    Base point for a nextjs typescripted candy-machine-ui

--- js/packages/candy-machine-ui/next.config.js
+++ js/packages/candy-machine-ui/next.config.js
@@ -1,31 +1,4 @@
-const shouldAnalyzeBundles = process.env.ANALYZE === 'true';
-
-let nextConfig = {
-  webpackDevMiddleware: config => {
-    config.watchOptions = {
-      poll: 1000,
-      aggregateTimeout: 300,
-    };
-    return config;
-  },
-  images: {
-    domains: ['via.placeholder.com'],
-  },
-  reactStrictMode: true,
-  compiler: {
-    styledComponents: true,
-  },
-  i18n: {
-    locales: ['en'],
-    defaultLocale: 'en',
-  },
+/** @type {import('next').NextConfig} */
+module.exports = {
+    reactStrictMode: true,
 };
-
-if (shouldAnalyzeBundles) {
-  const withBundleAnalyzer = require('@next/bundle-analyzer')({
-    enabled: process.env.ANALYZE === 'true',
-  });
-  nextConfig = withBundleAnalyzer(nextConfig);
-}
-
-module.exports = nextConfig;
